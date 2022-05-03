c0cae72ff4facf96d9e7ba9c8adb18a3ffe44b2a
James McHenry <kjamesjoseph@hotmail.com>
2022-05-03 17:30:53 -0400

    setting up the begging idea for are code

---
+++ Server/StartMirror.py
@@ -0,0 +1,34 @@
+import multiprocessing
+
+'''
+This file should be set to start up on powering of device
+'''
+
+def setUpScreenselector():
+    """
+    put code to call and set up screen
+    """
+    pass
+
+def setUpServer():
+    """
+    put code to call flask
+    """
+    pass
+
+def startup():
+    """
+    This is used to start up the app
+    """
+    p1 = multiprocessing.Process(target=setUpScreenselector(), args=())
+    p2 = multiprocessing.Process(target=setUpServer(), args=())
+    p1.start()
+    p2.start()
+    p2.join()
+    exit()
+
+if __name__=="__main__":
+    try:
+        startup()
+    except KeyboardInterrupt:
+        exit()
