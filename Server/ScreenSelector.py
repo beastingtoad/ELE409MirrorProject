bec8d996f5ad00303b07c891c2dfdd6aece40bdd
James McHenry <kjamesjoseph@hotmail.com>
2022-05-04 17:30:31 -0400

    updating progress of the buttons

--- Server/ScreenSelector.py
+++ Server/ScreenSelector.py
@@ -1,15 +1,27 @@
 import tkinter as tk
-
+from screeninfo import get_monitors
+for m in get_monitors():
+    print(str(m))
 
 class DisplayScreen(tk.Tk):
     def __init__(self):
         self.tempFlag = False
         super(DisplayScreen, self).__init__()
-        self.title("Mirror Screen Opstions")
         self.fullscreen()
         self.bind("<Escape>", self.fullscreen_exit)
+        self.title("Mirror Screen Opstions")
+        for m in get_monitors():
+            objx=int(m.width/3)
+        options=[0,1,2]
+        self.grid_columnconfigure(options,weight=objx)
+        #self.grid_rowconfigure(self,)
         self.TopLable = tk.Label(self, text="Display", font=('8514oem', 25, 'bold'))
-        self.TopLable.grid(row=0,column=0,columnspan=5,pady=10)
+        self.TopLable.grid(row=0,column=0,columnspan=3)
+        self.screen =[0,1,2,3,4,5]
+        for x in range(0,len(self.screen)):
+            self.screen[x]= tk.Button(self, text="Screen", font=('8514oem', 25, 'bold'))
+            self.screen[x].config(text="Screen"+str(x+1))
+            self.screen[x].grid(row=2+int(x/3), column=(x%3),sticky='we')
 
 
     def fullscreen(self,event=None):
