2dda253dae16cb0484cdae95a773c828041f2f5f
JamesMcHenry <kjamesjoseph@hotmail.com>
2022-05-05 01:09:52 -0400

    this is all that can be done for the front end for now for the screen this now can selcet a screen and the user knows witch is selceted

--- Server/ScreenSelector.py
+++ Server/ScreenSelector.py
@@ -19,15 +19,21 @@ class DisplayScreen(tk.Tk):
             objy = int(m.height / 3)
         options = [0, 1, 2]
         self.grid_columnconfigure(options, weight=objx)
+        options = [0, 1, 2, 3, 4]
         self.grid_rowconfigure(options, weight=objy)
         self.TopLable = tk.Label(self, text="Choose A Display", font=('8514oem', 50, 'bold'), bg='#3f4563',fg='#4ed3bb')
         self.TopLable.grid(row=0, column=0, columnspan=3)
         self.screen = [0, 1, 2, 3, 4, 5]
-        for n,x in enumerate(self.screen): #n is the index #x is the object
-            x = tk.Button(self, text="Screen", font=('8514oem', 25, 'bold'), bg='#3f4563', fg='#4ed3bb')
-            x.config(text="Screen" + str(n + 1), command= lambda n=n:self.screen_set(n))
-            x.grid(row=1 + int(n / 3), column=(n % 3), sticky='wens', padx=4, pady=4)
+        self.outline= [0, 1, 2, 3, 4, 5]
 
+        for n,x in enumerate(self.screen): #n is the index #x is the object
+            self.outline[n] = tk.Frame(self,bg= '#3f4563')
+            x = tk.Button(self.outline[n], text="Screen", font=('8514oem', 25, 'bold'), bg='#3f4563', fg='#4ed3bb')
+            x.config(text="Screen" + str(n + 1), command= lambda n=n:self.screen_set(n),borderwidth=40,border=True,)
+            x.pack(padx=4, pady=4,fill='both',expand=True)
+            rmath=1 + int(n / 3)*2
+            print(rmath)
+            self.outline[n].grid(row = rmath, column = (n % 3),rowspan=2, sticky = 'wesn')
     def fullscreen(self, event=None):
         '''
         sets screen to fullscreen
@@ -48,7 +54,7 @@ class DisplayScreen(tk.Tk):
         '''
         for x in range(0, len(self.SetScreen)):
             self.SetScreen[x] = True if x == screennum else False
-
+            self.outline[x].config(bg = '#4ed3bb' if x == screennum else '#3f4563')
     def retrive_screen_state(self) -> list[bool]:
         '''
         used to retrive the list of screen states
